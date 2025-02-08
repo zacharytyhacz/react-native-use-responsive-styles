9c6d9ea1798d6fd2baa0934ed0f4dc714bb65d4c
zacharytyhacz <zactyh@gmail.com>
2025-02-08 17:18:28 -0500

    feat(hook): initial useResponsiveStyles hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,47 @@
-export function multiply(a: number, b: number): number {
-  return a * b;
+import { useMemo } from 'react';
+
+import {
+  useWindowDimensions,
+  StyleSheet,
+  type StyleProp,
+  type ViewStyle,
+  type TextStyle,
+  type ImageStyle,
+} from 'react-native';
+
+type Props = {
+  shared?: StyleProp<ViewStyle | TextStyle | ImageStyle>;
+  mobile?: StyleProp<ViewStyle | TextStyle | ImageStyle>;
+  tablet?: StyleProp<ViewStyle | TextStyle | ImageStyle>;
+  desktop?: StyleProp<ViewStyle | TextStyle | ImageStyle>;
+};
+
+export function useResponsiveStyles(
+  props: Props
+): StyleProp<ViewStyle | TextStyle | ImageStyle> {
+  const { width } = useWindowDimensions();
+
+  const styles = useMemo(() => {
+    const { shared, mobile, tablet, desktop } = props;
+
+    const stylesToBe = [shared];
+
+    if (width < 768) {
+      stylesToBe.push(mobile);
+    }
+
+    if (width >= 768 && width < 1024) {
+      stylesToBe.push(tablet);
+    }
+
+    if (width >= 1024) {
+      stylesToBe.push(desktop);
+    }
+
+    const style = StyleSheet.flatten(stylesToBe);
+
+    return style;
+  }, [props, width]);
+
+  return styles;
 }
